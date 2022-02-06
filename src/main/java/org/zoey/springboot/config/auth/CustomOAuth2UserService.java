727815894c8610941c4e957e2e58140b0a45645a
유승아 <aaay8228@gmail.com>
2022-02-06 17:30:36 +0900

    test

---
+++ src/main/java/org/zoey/springboot/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,11 @@
+package org.zoey.springboot.config.auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+// service 역할을 하는 class임을 알린다.
+@Service // 비지니스 로직을 수행한다.
+public class CustomOAuth2UserService {
+}
